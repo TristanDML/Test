fe5f6de956b11d44ac531b828a52954e2ad46105
TristAot <cytshilebe@gmail.com>
2021-12-27 19:39:46 +0100

    zzzz

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,6 @@ import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 import { hotjar } from 'react-hotjar'
 
-import * as ga from '../lib/ga'
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter()
